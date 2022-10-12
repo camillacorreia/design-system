33e372c6fc1c8c7d2dd891ffb38ea3199cc096a9
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-10-12 18:29:46 -0300

    feat: configuration storybook and tokens tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,39 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#FFF',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c8a',
+        200: '#c4c4cc',
+        100: '#e1e1e6',
+      },
+
+      pantone: {
+        500: '#f2a900',
+        300: '#ffc236'
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
   },
   plugins: [],
 }
